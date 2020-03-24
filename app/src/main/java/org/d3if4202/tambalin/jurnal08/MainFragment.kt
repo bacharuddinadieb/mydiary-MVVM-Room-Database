5cea097ed830ff6ee45555661e43eb5a414ec983
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-03-24 13:49:48 +0700

    onclick fab

--- app/src/main/java/org/d3if4202/tambalin/jurnal08/MainFragment.kt
+++ app/src/main/java/org/d3if4202/tambalin/jurnal08/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import org.d3if4202.tambalin.jurnal08.databinding.FragmentMainBinding
 
 /**
@@ -19,6 +20,10 @@ class MainFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
         setHasOptionsMenu(true)
+
+        binding.fabTambahDiary.setOnClickListener {
+            it.findNavController().navigate(R.id.action_mainFragment_to_fragmentAddDiary)
+        }
 
         return binding.root
     }
