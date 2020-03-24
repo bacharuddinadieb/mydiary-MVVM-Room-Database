dedfa57a59ac5076c2736c23dacdef6263e1d845
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-03-24 13:47:34 +0700

    menampilkan menu

--- app/src/main/java/org/d3if4202/tambalin/jurnal08/FragmentAddDiary.kt
+++ app/src/main/java/org/d3if4202/tambalin/jurnal08/FragmentAddDiary.kt
@@ -1,22 +1,31 @@
 package org.d3if4202.tambalin.jurnal08
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import org.d3if4202.tambalin.jurnal08.databinding.FragmentAddDiaryBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class FragmentAddDiary : Fragment() {
+    lateinit var binding: FragmentAddDiaryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_diary, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_diary, container, false)
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_add, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
 }
--- app/src/main/java/org/d3if4202/tambalin/jurnal08/MainFragment.kt
+++ app/src/main/java/org/d3if4202/tambalin/jurnal08/MainFragment.kt
@@ -1,22 +1,31 @@
 package org.d3if4202.tambalin.jurnal08
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import org.d3if4202.tambalin.jurnal08.databinding.FragmentMainBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class MainFragment : Fragment() {
+    lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_main, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
 }
